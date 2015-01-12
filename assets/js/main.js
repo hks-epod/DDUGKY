05bb13939bb879a16ce234fb994038dab3be53cf
Ravi Suhag <suhag.ravi@gmail.com>
2015-01-13 00:07:49 +0530

    Finishing loading from spreadsheet and makers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,63 @@
 function initialize() {
 
-  var mapOptions = {
-    center: {
-      lat: 21.0000 ,
-      lng: 78.0000
-    },
-    zoom: 5
-  };
-
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-
-
-  var myLatlng = new google.maps.LatLng(28.6100, 77.2300);
-  var data = {
-    screen_name: "Ravi Suhag",
-    text: "Here is the name"
-  };
-  var contentString = '<div id="content">' +
-    '<div id="siteNotice">' +
-    '</div>' +
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-    '<div id="bodyContent">' +
-    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    'sandstone rock formation in the southern part of the ' +
-    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-    'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-    'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-    'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-    'Aboriginal people of the area. It has many springs, waterholes, ' +
-    'rock caves and ancient paintings. Uluru is listed as a World ' +
-    'Heritage Site.</p>' +
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-    'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-    '(last visited June 22, 2009).</p>' +
-    '</div>' +
-    '</div>';
-
-  // Add Markers
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    map: map,
-    animation: google.maps.Animation.DROP,
-    title: "Hello World!"
-  });
+    // Assign varibales and options
+    var mapOptions = {
+      center: {
+        lat: 21.0000,
+        lng: 78.0000
+      },
+      zoom: 5
+    };
+    var infowindow = new google.maps.InfoWindow();
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
 
-  // Attach info window to marker
+    var ds = new Miso.Dataset({
+      importer: Miso.Dataset.Importers.GoogleSpreadsheet,
+      parser: Miso.Dataset.Parsers.GoogleSpreadsheet,
+      key: "1ucbsCdlLWjIy2gfUIiMHje5cJY7qlxHVkSsRfellqhY",
+      worksheet: "1"
+    });
 
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
+    ds.fetch({
+      success: function() {
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map, marker);
-  });
+
+        console.log(ds.columnNames());
+        addMarker();
 
 
 
-}
 
 
+
+      },
+      error: function() {
+        console.log("Are you sure you are connected to the internet?");
+      }
+    });
+
+
+    function addMarker() {
+      var myLatlng = new google.maps.LatLng(28.6100 + i / 1, 77.2300);
+      var contentString = 'Hello';
+      // Add Markers
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: "Hello World!"
+      });
+      // Attach info window to marker
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.close();
+        infowindow.setContent(contentString);
+        infowindow.open(map, marker);
+      });
+    }
+
+
+
+
+  } // initialize
 var map = google.maps.event.addDomListener(window, 'load', initialize);
