a22cd71bffc034b629cd075c018d97a86d19bf96
Ravi Suhag <suhag.ravi@gmail.com>
2015-01-13 15:41:32 +0530

    Overlapping marker issue resolved

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,17 @@ function initialize() {
     };
     var infowindow = new google.maps.InfoWindow();
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    // Overalapping marker Assign
+    var oms = new OverlappingMarkerSpiderfier(map);
+
+    oms.addListener('click', function(marker, event) {
+      infowindow.setContent(marker.content);
+      infowindow.open(map, marker);
+    });
+
+    oms.addListener('spiderfy', function(markers) {
+      infowindow.close();
+    });
 
 
     var ds = new Miso.Dataset({
@@ -22,7 +33,7 @@ function initialize() {
     ds.fetch({
       success: function() {
         this.each(function(row) {
-          addMarker(row);
+          loadMarker(row);
         });
       },
       error: function() {
@@ -31,13 +42,13 @@ function initialize() {
     });
 
 
-    function addMarker(tcenter) {
+    function loadMarker(tcenter) {
       var myLatlng = new google.maps.LatLng(tcenter.Latitude, tcenter.Longitude);
-      
+
       function contentString() {
         var content = '';
         for (var key in tcenter) {
-          content+= '<dt>' + key + '</dt><dd>' + tcenter[key] + '</dd>' ;
+          content += '<dt>' + key + '</dt><dd>' + tcenter[key] + '</dd>';
         }
         return content
       }
@@ -45,7 +56,7 @@ function initialize() {
         '<div id="siteNotice">' +
         '</div>' +
         '<h3>' + tcenter['PIA name'] + '</h3>' +
-        '<dl class="dl-horizontal">' + contentString() +  '</dl>' +
+        '<dl class="dl-horizontal">' + contentString() + '</dl>' +
         '</div>' +
         '</div>';
 
@@ -55,14 +66,11 @@ function initialize() {
         position: myLatlng,
         map: map,
         animation: google.maps.Animation.DROP,
-        title: "Hello World!"
+        content: contentString
+
       });
-      // Attach info window to marker
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.close();
-        infowindow.setContent(contentString);
-        infowindow.open(map, marker);
-      });
+      oms.addMarker(marker);
+
     }
 
 
@@ -70,3 +78,72 @@ function initialize() {
 
   } // initialize
 var map = google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
+
+// jQuery(document).ready(function($) {
+//   var oms;
+
+//   var MarkerData = {
+//     "lat": 0,
+//     "lng": 0,
+//     "data": "Overlay text here."
+//   };
+
+//   $('#selector').gmap3({
+//     action: 'init',
+//     options: {
+//       scaleControl: false,
+//       center: [52.132633, 5.291266],
+//       zoom: 7,
+//       mapTypeControl: false,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP
+//     },
+//     callback: function(map) {
+//       oms = new OverlappingMarkerSpiderfier(map, {
+//         markersWontMove: true,
+//         markersWontHide: true,
+//         keepSpiderfied: true,
+//         nearbyDistance: 10,
+//         legWeight: 5
+//       });
+//     }
+//   }, {
+//     action: 'addMarkers',
+//     markers: MarkerData,
+//     marker: {
+//       options: {
+//         icon: new google.maps.MarkerImage('http://www.example.com/icon.png')
+//       }
+//     },
+//     callback: function(markers) {
+//       $.each(markers, function(i, marker) {
+//         marker.data = MarkerData[i].data;
+
+//         oms.addMarker(marker);
+//       });
+//     }
+//   });
+
+//   oms.addListener('click', function(marker) {
+//     $('#selector').gmap3({
+//       action: 'clear',
+//       name: 'overlay'
+//     }, {
+//       action: 'addOverlay',
+//       latLng: marker.getPosition(),
+//       content: '<div class="map-overlay">' + marker.data + '</div>',
+//       offset: {
+//         x: -30,
+//         y: 0
+//       }
+//     }, {
+//       action: 'panTo',
+//       args: [marker.position]
+//     });
+//   });
+// });
